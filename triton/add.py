624acf8dfdebf2753fa77fe21a2f1947c3591197
Nikhil Chowdary Paleti <68870951+Nikhil-Paleti@users.noreply.github.com>
2025-05-25 10:46:29 -0700

    Update add.py

--- triton/add.py
+++ triton/add.py
@@ -37,8 +37,40 @@ def test_add_kernel(size, atol=1e-3, rtol=1e-3, device=DEVICE):
     torch.testing.assert_close(z_tri, z_ref, atol=atol, rtol=rtol)
     print('PASSED')
 
+@triton.testing.perf_report(
+    triton.testing.Benchmark(
+        x_names=['size'],
+        x_vals = [2**i for i in range(12, 28, 1)],
+        x_log = True,
+        line_arg='provider',
+        line_vals=['triton', 'pytorch'],
+        line_names=['Triton', 'PyTorch'],
+        styles=[('blue', '-'), ('green', '-')],
+        ylabel='GB/s',
+        plot_name='vector-add-performance',
+        args = {}
+    )
+)
+def benchmark(size, provider):
+    x = torch.rand(size, device=DEVICE, dtype=torch.float32)
+    y = torch.rand(size, device=DEVICE, dtype=torch.float32)
+     
+    quantiles = [0.5, 0.05, 0.95]
+    if provider == 'pytorch':
+        ms, min_ms, max_ms = triton.testing.do_bench(lambda: x+y , quantiles=quantiles)
+    if provider == 'triton':
+        ms, min_ms, max_ms = triton.testing.do_bench( lambda: add(x,y) , quantiles=quantiles)
+         
+    gbps = lambda ms: 3 * x.numel() * x.element_size() * 1e-9 / (ms * 1e-3) 
+
+    return gbps(ms), gbps(max_ms), gbps(min_ms)
+
 if __name__ == '__main__':
     test_add_kernel(2048)
     test_add_kernel(9078)
     test_add_kernel(15000)
-    
+
+    import sys 
+
+    if len(sys.argv) > 1 and sys.argv[1] == '--benchmark':
+        benchmark.run(save_path='./benchmark/', print_data=True)
